a0eb5630c8ceb84bd0f22d054c22deea34873a04
TheK4 <talesaknauer@gmail.com>
2025-02-15 14:46:58 -0300

    first commit (navbar section create)

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Navbar from "./components/Navbar.js"
+
+const App = () => {
+  return(
+    <div className=" container mx-auto px-8 ">
+      <Navbar />
+    </div>
+  );
+};
+
+export default App
